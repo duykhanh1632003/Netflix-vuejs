ba8121309c06c9ffc88bf32a8509b9beb84aedd3
Nguyễn Duy Khánh <109708814+duykhanh1632003@users.noreply.github.com>
2023-09-26 23:05:59 +0700

    Thêm code ban đầu

---
+++ layouts/MainLayout.vue
@@ -0,0 +1,42 @@
+<template>
+    <div id="MainLayout" class="w-full fixed z-50">
+        <div id="TopMenu" class="w-full bg-[#FAFAFA] border-b mb:    hidden">
+            <ul class="
+        flex
+        items-center
+        justify-end
+        text-xs
+        text-[#333333]
+        font-light
+        px-2
+        h-10
+        bg-[#FAFAFA]
+        max-w-[1200px]
+      ">
+
+                <li class="border-r border-r-gray-400 px-3 hover:text-[#FF4646] cursor-pointer">
+                    sell
+                </li>
+                <li class="border-r border-r-gray-400 px-3 hover:text-[#FF4646] cursor-pointer">
+                    Cookie
+                </li>
+                <li class="border-r border-r-gray-400 px-3 hover:text-[#FF4646] cursor-pointer">
+                    Help
+                </li>
+                <li class="border-r border-r-gray-400 px-3 hover:text-[#FF4646] cursor-pointer">
+                    Buyer
+                </li>
+
+                <li class="px-3 hover:text-[#FF4646] cursor-pointer">
+                    <Icon name="ic:sharp-install-mobile" size="17" />
+                    App
+                </li>
+            </ul>
+        </div>
+        <slot></slot>
+    </div>
+</template>
+
+<script setup>
+
+</script>
